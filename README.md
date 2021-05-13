0a27364333e86b60dc65bc39dc00a576787b7772
Jason Dempsey <jasondemps1@gmail.com>
2021-05-13 13:41:26 +0000

    Websocket to binance API works for a given symbol

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Hedgehog
+# Crypto Buddy
 
-**TODO: Add description**
+## Description
+Currently connects to Binance API via a WebSocket and receives trade events. Decodes the JSON trade events and logs a message.
 
 https://www.elixircryptobot.com/stream-live-crypto-prices-from-binance-wss.html#objectives
--- apps/streamer/lib/streamer/binance.ex
+++ apps/streamer/lib/streamer/binance.ex
@@ -1,5 +1,6 @@
 defmodule Streamer.Binance do
   use WebSockex
+  require Logger
 
   @stream_endpoint "wss://stream.binance.com:9443/ws/"
 
@@ -9,17 +10,43 @@ defmodule Streamer.Binance do
     WebSockex.start_link(
       "#{@stream_endpoint}#{symbol}@trade",
       __MODULE__,
-      nil)
+      nil
+    )
   end
 
-  def handle_frame({type, msg}, state) do
-    IO.puts "Received Message - Type: #{inspect type} -- Message: #{inspect msg}"
+  def handle_frame({_type, msg}, state) do
+    # IO.puts "Received Message - Type: #{inspect type} -- Message: #{inspect msg}"
+    case Jason.decode(msg) do
+      {:ok, event} -> process_event(event)
+      {:error, _} -> Logger.error("Unable to parse msg: #{msg}")
+    end
+
     {:ok, state}
   end
 
+  defp process_event(%{"e" => "trade"} = event) do
+    trade_event = %Streamer.Binance.TradeEvent{
+      :event_type => event["e"],
+      :event_time => event["E"],
+      :symbol => event["s"],
+      :trade_id => event["t"],
+      :price => event["p"],
+      :quantity => event["q"],
+      :buyer_order_id => event["b"],
+      :seller_order_id => event["a"],
+      :trade_time => event["T"],
+      :buyer_market_maker => event["m"]
+    }
+
+    Logger.debug(
+      "Trade event received" <>
+        "#{trade_event.symbol}@#{trade_event.price}"
+    )
+  end
+
   # Not sending any messages to Binance, we dont need this right now.
-  #def handle_cast({:send, {type, msg} = frame}, state) do
-    #IO.puts "Sending #{type} frame with payload: #{msg}"
-    #{:reply, frame, state}
-  #end
+  # def handle_cast({:send, {type, msg} = frame}, state) do
+  # IO.puts "Sending #{type} frame with payload: #{msg}"
+  # {:reply, frame, state}
+  # end
 end
